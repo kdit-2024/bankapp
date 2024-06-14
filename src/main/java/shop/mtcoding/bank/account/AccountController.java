3341dde86195b0daf97af47393b60522d6039873
getinthere <getinthere@naver.com>
2024-06-14 12:31:09 +0900

    계좡 목록보기 완료

--- src/main/java/shop/mtcoding/bank/account/AccountController.java
+++ src/main/java/shop/mtcoding/bank/account/AccountController.java
@@ -1,13 +1,13 @@
 package shop.mtcoding.bank.account;
 
+import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpSession;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.*;
 import shop.mtcoding.bank.user.User;
+
+import java.util.List;
 
 
 // 스프링이 관리하는 객체가 됨
@@ -31,11 +31,34 @@ public class AccountController {
 
     //@RequestMapping(method = RequestMethod.GET, value = "/home")
     @GetMapping("/account/list")
-    public String accountList(){
+    public String accountList(HttpServletRequest request){
         User sessionUser = (User) session.getAttribute("sessionUser");
         if(sessionUser == null) throw new RuntimeException("인증되지 않은 사용자입니다.");
 
+        AccountResponse.ListDTO resDTO = accountService.나의계좌목록(sessionUser.getId());
+        request.setAttribute("model", resDTO);
+
         return "account/list"; // templates/list.mustache 파일을 읽어서 응답
+    }
+
+    @GetMapping("/account/list/v2")
+    public @ResponseBody AccountResponse.ListDTO accountListV2(){
+        User sessionUser = (User) session.getAttribute("sessionUser");
+        if(sessionUser == null) throw new RuntimeException("인증되지 않은 사용자입니다.");
+
+        AccountResponse.ListDTO resDTO = accountService.나의계좌목록(sessionUser.getId());
+
+        return resDTO;
+    }
+
+    @GetMapping("/account/list/v3")
+    public @ResponseBody List<Account> accountListV3(){
+        User sessionUser = (User) session.getAttribute("sessionUser");
+        if(sessionUser == null) throw new RuntimeException("인증되지 않은 사용자입니다.");
+
+        List<Account> resDTO = accountService.나의계좌목록V3(sessionUser.getId());
+
+        return resDTO;
     }
 
     @GetMapping("/account/save-form")
